c9d126452253147eae6c4d952871544814a387ed
Hiroyasu Nakano <109708655+Nakano-Hi@users.noreply.github.com>
2022-07-24 18:17:06 +0900

    Delete Ant/experiment_Ant/Ant_submit_constant_proposed directory

--- Ant/experiment_Ant/Ant_submit_constant_proposed/Exploration_Phase.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def pick_sample(K,theta,Sigma):
-    theta_list = theta.tolist()
-    Sigma_list = Sigma.tolist()
-    theta_test=np.random.multivariate_normal(theta_list, Sigma_list, K)
-    
-    return theta_test
